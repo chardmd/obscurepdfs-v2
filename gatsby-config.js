e3c38e1ed3fe6739fc7c58a8ef7d8016498b4d02
Richard Dimalanta <chardmd.au@gmail.com>
2020-07-11 18:09:19 +1000

    removed unused plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
         name: "assets",
       },
     },
-    // content of the markdown
+    // data - markdown content
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -54,24 +54,6 @@ module.exports = {
         plugins: [
           {
             resolve: "gatsby-remark-relative-images",
-          },
-          {
-            resolve: "gatsby-remark-images",
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-
-              maxWidth: 590,
-            },
-          },
-          // Copies local files linked to/from Markdown (.md|.markdown) files
-          // to the root directory (i.e., public folder).
-          {
-            resolve: "gatsby-remark-copy-linked-files",
-            options: {
-              destinationDir: "static",
-            },
           },
         ],
       },
