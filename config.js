1665ae63112055ec54b50bb36b6b866d758744c7
Richard Dimalanta <chardmd.au@gmail.com>
2020-07-12 11:33:20 +1000

    updated analtyics id

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   siteUrl: "https://www.obscurepdfs.com",
   author: "Obscure PDFs",
   copyright: "2020 © All rights reserved.",
-  googleAnalyticsId: "UA-168810586-1",
+  googleAnalyticsId: "UA-165978811-1",
   policyDate: "18 July 2020.",
   email: "hello@obscurepdfs.com",
 };
