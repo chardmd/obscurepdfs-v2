95439d133c4004b9815a4d301308394bab63b2ba
Richard Dimalanta <chardmd.au@gmail.com>
2020-07-12 15:04:56 +1000

    updated css

--- src/components/Post-Detail/Description/Description.js
+++ src/components/Post-Detail/Description/Description.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // react-bootstrap
-import { Button, Badge } from "react-bootstrap";
+import { Badge } from "react-bootstrap";
 
 import getDomain from "../../../utils/get-domain";
 
@@ -9,24 +9,13 @@ import styles from "./Description.module.scss";
 
 const Description = ({ category, url }) => (
   <div className={styles.description}>
-    <div className={styles.wrapper}>
+    <div className={styles.download}>
       <h5>
         <Badge pill variant="info" className={styles.likes}>
           {category}
         </Badge>
       </h5>
-    </div>
-    <div className={styles.download}>
-      <div className={styles.mainText}>Click the button to view</div>
       <div className={styles.subText}>source: {getDomain(url, true)}</div>
-      <Button
-        href={url}
-        rel="nofollow noopener noreferrer"
-        target="_blank"
-        variant="success"
-      >
-        View PDF
-      </Button>
     </div>
   </div>
 );
--- src/components/Post-Detail/Post-Detail.js
+++ src/components/Post-Detail/Post-Detail.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Img from "gatsby-image";
 
+// react-bootstrap
+import { Button } from "react-bootstrap";
+
 import Userbar from "./Userbar";
 import Description from "./Description";
 import Share from "../Share";
@@ -18,7 +21,23 @@ const PostDetail = ({ collectionDetail }) => {
     <div onClick={(e) => e.stopPropagation()} className={styles.postDetail}>
       <div className={styles.mainWrapper}>
         <Userbar title={title} />
-        <Share slug={slug} title={title} options={{ size: 28, round: true }} />
+        <div className={styles.headWrapper}>
+          <Share
+            slug={slug}
+            title={title}
+            options={{ size: 28, round: true }}
+          />
+          <Button
+            size="sm"
+            href={url}
+            rel="nofollow noopener noreferrer"
+            target="_blank"
+            variant="success"
+            className="mb-2"
+          >
+            View PDF
+          </Button>
+        </div>
         <div className={styles.detailWrapper}>
           <Description category={category} url={url} />
         </div>
